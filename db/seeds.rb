42dc47dd298b5f1382e19826d67575a14464aaaa
Nikhil Narayen <nnarayen@berkeley.edu>
2015-09-23 22:21:46 -0700

    Updating arclint to point to rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 def make_partners
-	1.upto(5) do |n|
-		partner = Partner.create({ email: "user#{n}@gmail.com", first_name:
-				"Nikhil", last_name: "Narayen" })
-		partner.save
-	end
+  1.upto(5) do |n|
+    partner = Partner.create(email: "user#{n}@gmail.com", first_name: "Nikhil", last_name: "Narayen")
+    partner.save
+  end
 end
 
 make_partners
