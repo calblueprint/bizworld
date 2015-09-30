005ca6e27ac5a0811b3477953b1d7362c454439e
Tricia Fu <triciasfu@gmail.com>
2015-09-29 20:02:42 -0700

    Add Teacher Model
    Summary: Teacher Model with email and password
    
    Test Plan: run rake db:migrate
    
    Reviewers: nnarayen
    
    Reviewed By: nnarayen
    
    Subscribers: tofu, liang, generic, caseytaco
    
    Projects: #bizworld
    
    Differential Revision: http://phab.calblueprint.org/D17

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
+  devise_for :teachers
   resources :partners
-
   root to: "partners#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,4 +48,22 @@ ActiveRecord::Schema.define(version: 20150930022225) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teachers", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true
+  add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
+
 end
