61c8c0cba661303eb1f10eed60ace97a774e88e8
Nikhil Narayen <nnarayen@berkeley.edu>
2015-09-29 21:05:04 -0700

    Fixing indenting
    Summary: Same as above
    
    Test Plan: Pls look
    
    Reviewers: tofu
    
    Reviewed By: tofu
    
    Subscribers: liang, tofu, generic, caseytaco
    
    Projects: #bizworld
    
    Differential Revision: http://phab.calblueprint.org/D22

--- app/assets/javascripts/components/partners.jsx
+++ app/assets/javascripts/components/partners.jsx
@@ -5,35 +5,35 @@ var React = require('react');
  */
 var PartnerList = React.createClass({
     getInitialState: function() {
-	return {
-	    partners: this.props.partners,
-	};
+        return {
+            partners: this.props.partners,
+        };
     },
     render: function() {
-	var partners = this.state.partners.map(function(partner) {
-	    return (
-		<Partner partner  = {partner}
-		         key      = {partner["id"]} />
-	    );
-	});
-	return (
-	    <div className="row">
-		<div className="col-md-8">
-		    <table className="table">
-			<thead>
-			    <tr>
-				<th>Partner</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-			    </tr>
-			</thead>
-			<tbody>
-			    {partners}
-			</tbody>
-		    </table>
-		</div>
-	    </div>
-	);
+        var partners = this.state.partners.map(function(partner) {
+            return (
+                <Partner partner  = {partner}
+                         key      = {partner["id"]} />
+            );
+        });
+        return (
+            <div className="row">
+                <div className="col-md-8">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Partner</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {partners}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
     }
 });
 
@@ -42,24 +42,24 @@ var PartnerList = React.createClass({
  */
 var Partner = React.createClass({
     getInitialState: function() {
-	return {
-	    partner: this.props.partner,
-	};
+        return {
+            partner: this.props.partner,
+        };
     },
     render: function() {
-	return (
-	    <tr>
-		<td>
-		    { this.state.partner["email"] }
-		</td>
-		<td>
-		    { this.state.partner["first_name"]}
-		</td>
-		<td>
-		    { this.state.partner["last_name"] }
-		</td>
-	    </tr>
-	);
+        return (
+            <tr>
+                <td>
+                    { this.state.partner["email"] }
+                </td>
+                <td>
+                    { this.state.partner["first_name"]}
+                </td>
+                <td>
+                    { this.state.partner["last_name"] }
+                </td>
+            </tr>
+        );
     }
 });
 
