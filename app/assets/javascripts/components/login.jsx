3756e7d4444fdaa319c761dfb67579d3f4ab3f9c
Eric Liang <liang.eric24@gmail.com>
2015-10-27 01:24:58 -0700

    organized CSS and styled login page
    Summary: created mixins and variables scss, made login page pretty
    
    Test Plan: check css folder organization, check login page new styles
    
    Reviewers: nnarayen
    
    Reviewed By: nnarayen
    
    Subscribers: bigblue, tofu, generic, caseytaco
    
    Projects: #bizworld
    
    Differential Revision: http://phab.calblueprint.org/D48

--- app/assets/javascripts/components/login.jsx
+++ app/assets/javascripts/components/login.jsx
@@ -1,56 +1,58 @@
 class LoginPage extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  _attemptLogin = (e) => {
-    $.ajax({
-      url: "/sign_in",
-      type: "POST",
-      dataType: "json",
-      data: {
-        "teacher" : {
-          "email"    : $("input[name=email]").val() ,
-          "password" : $("input[name=password]").val()
-        }
-      },
-      success: (msg) => {
-        toastr.success(msg.message);
-        window.location.replace(msg.to);
-      },
-      error: (xhr, status, error) => {
-        toastr.error(JSON.parse(xhr.responseText).message);
-      }
-    });
-  }
-
-  _handleKeydown = (e) => {
-    if (e.which == 13) {
-      this._attemptLogin();
+    constructor(props) {
+        super(props);
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <form>
-          <div className="input-container">
-            <input name="email" type="text" placeholder="Email address"
-              onKeyDown={this._handleKeydown} autoFocus />
-          </div>
-          <div className="input-container">
-            <input name="password" type="password" placeholder="Password"
-              onKeyDown={this._handleKeydown} />
-          </div>
-          <div className="input-container">
-            <input name="submit" type="button" value="Login"
-              onClick={this._attemptLogin} />
-          </div>
-        </form>
-      </div>
-    );
-  }
+    _attemptLogin = (e) => {
+        $.ajax({
+            url: "/sign_in",
+            type: "POST",
+            dataType: "json",
+            data: {
+                "teacher" : {
+                    "email"    : $("input[name=email]").val() ,
+                    "password" : $("input[name=password]").val()
+                }
+            },
+            success: (msg) => {
+                toastr.success(msg.message);
+                window.location.replace(msg.to);
+            },
+            error: (xhr, status, error) => {
+                toastr.error(JSON.parse(xhr.responseText).message);
+            }
+        });
+    }
+
+    _handleKeydown = (e) => {
+        if (e.which == 13) {
+            this._attemptLogin();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <fieldset className="input-container">
+                        <label>Email</label>
+                        <input name="email" type="text"
+                            onKeyDown={this._handleKeydown} autoFocus />
+                    </fieldset>
+                    <fieldset className="input-container">
+                        <label>Password</label>
+                        <input name="password" type="password"
+                            onKeyDown={this._handleKeydown} />
+                    </fieldset>
+                    <fieldset className="input-container">
+                        <input name="submit" type="button" value="Login" className="submit-btn"
+                            onClick={this._attemptLogin} />
+                    </fieldset>
+                </form>
+            </div>
+        );
+    }
 }
 
 LoginPage.propTypes = { };
