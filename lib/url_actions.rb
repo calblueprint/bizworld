d655371ba3c672dfa0c7009b460e297ccebda85f
Aleks Kamko <aykamko@gmail.com>
2016-02-15 14:57:33 -0800

    Disable Bitly on dev and test environments
    Summary:
    See title. Form links look bad, but only locally.
    
    Screenshot:
    {F13459}
    
    Fixes T35
    
    Test Plan: N/A
    
    Reviewers: nnarayen
    
    Reviewed By: nnarayen
    
    Subscribers: liang, tofu, generic, caseytaco
    
    Projects: #bizworld
    
    Maniphest Tasks: T35
    
    Differential Revision: http://phab.calblueprint.org/D196

--- lib/url_actions.rb
+++ lib/url_actions.rb
@@ -1,5 +1,8 @@
 module UrlActions
   def self.shorten_url(classroom_id, category)
+    if Rails.env.development? || Rails.env.test?
+      return "/classrooms/#{classroom_id}/forms/#{category}"
+    end
     Bitly.client.shorten("#{ENV['APP_URL']}/classrooms/#{classroom_id}/forms/#{category}").short_url
   end
 end
