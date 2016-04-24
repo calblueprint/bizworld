260e1c5918baec902f6bc9f4c65c1813f5b51aaa
nishdesai <nish.sdesai@gmail.com>
2016-04-23 19:19:25 -0700

    Merge from remote

--- app/assets/javascripts/components/teachers/editable_select.jsx
+++ app/assets/javascripts/components/teachers/editable_select.jsx
@@ -1,57 +1,57 @@
 /**
- * @prop label    - label of field to show
- * @prop data     - current input for select
+ * @prop label  - label of field to show
+ * @prop data   - current input for select
  * @prop editable - true if fields are editable
  * @prop multiple - whether this select can have multiple values
  */
 class EditableSelect extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.props.editable != nextProps.editable ||
+      this.props.data != nextProps.data);
+  }
+
+  componentDidUpdate(prevProps) {
+    // Workaround since selectpicker creates duplicate DOM elements
+    if (prevProps.editable) {
+      $('.bootstrap-select').remove();
+    }
+  }
+
+  render() {
+    var selectVal;
+    if (this.props.editable) {
+      if (this.props.multiple) {
+        selectVal = <GradesPicker grades = { this.props.data } />
+      } else {
+        selectVal = <StatePicker state = { this.props.data } />
+      }
+    } else {
+      selectVal = this.props.data;
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return (this.props.editable != nextProps.editable ||
-            this.props.data != nextProps.data);
-    }
-
-    componentDidUpdate(prevProps) {
-        // Workaround since selectpicker creates duplicate DOM elements
-        if (prevProps.editable) {
-            $('.bootstrap-select').remove();
-        }
-    }
-
-    render() {
-        var selectVal;
-        if (this.props.editable) {
-            if (this.props.multiple) {
-                selectVal = <GradesPicker grades = { this.props.data } />
-            } else {
-                selectVal = <StatePicker state = { this.props.data } />
-            }
-        } else {
-            selectVal = this.props.data;
-        }
-
-        return (
-            <fieldset className="input-container">
-                <div className="label-container">
-                    <label htmlFor={this.props.label}>
-                        { this.props.label }:
-                    </label>
-                </div>
-                <div className="input-box-container">
-                    { selectVal }
-                </div>
-            </fieldset>
-        );
-    }
+    return (
+      <fieldset className="input-container">
+        <div className="label-container">
+          <label htmlFor={this.props.label}>
+            { this.props.label }:
+          </label>
+        </div>
+        <div className="input-box-container">
+          { selectVal }
+        </div>
+      </fieldset>
+    );
+  }
 }
 
 EditableSelect.propTypes = {
-    data          : React.PropTypes.string,
-    label         : React.PropTypes.string.isRequired,
-    multiple      : React.PropTypes.bool.isRequired,
-    editable      : React.PropTypes.bool.isRequired
+  data     : React.PropTypes.string,
+  label     : React.PropTypes.string.isRequired,
+  multiple   : React.PropTypes.bool.isRequired,
+  editable   : React.PropTypes.bool.isRequired
 };
--- app/assets/javascripts/components/teachers/teachers.jsx
+++ app/assets/javascripts/components/teachers/teachers.jsx
@@ -1,93 +1,93 @@
 /**
  * @prop teacher_id - the id associated with the teacher
  * @prop program_id - the id associated with the selected program
- * @prop type       - active or inactive classroom
+ * @prop type    - active or inactive classroom
  */
 
-class TeacherModal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { classrooms : [], isLoading : true };
-    }
+ class TeacherModal extends React.Component {
+ constructor(props) {
+  super(props);
+  this.state = { classrooms : [], isLoading : true };
+ }
 
-    componentDidMount() {
-        this._fetchClassrooms(this.props);
-    }
+ componentDidMount() {
+  this._fetchClassrooms(this.props);
+ }
 
-    _fetchClassrooms(params) {
-        const success = (data) => { this.setState({ classrooms: data, isLoading: false }) }
-        APIRequester.getJSON(APIConstants.teachers.classrooms(this.props.teacher_id), success, params)
-    }
+ _fetchClassrooms(params) {
+  const success = (data) => { this.setState({ classrooms: data, isLoading: false }) }
+  APIRequester.getJSON(APIConstants.teachers.classrooms(this.props.teacher_id), success, params)
+ }
 
-    componentWillReceiveProps(nextProps) {
-        this._fetchClassrooms(nextProps);
-    }
+ componentWillReceiveProps(nextProps) {
+  this._fetchClassrooms(nextProps);
+ }
 
-    render() {
-        let classrooms;
-        let createCourse;
+ render() {
+  let classrooms;
+  let createCourse;
 
-        if (this.state.isLoading) {
-            if (this.props.type == 'active') {
-                classrooms = (
-                    <div className="spinner-container"></div>
-                )
-            }
-        } else {
-            classrooms = this.state.classrooms.map(function(classroom) {
-                return (
-                    <ClassroomContainer classroom = {classroom}
-                                              key = {classroom["id"]} />
-                );
-            });
+  if (this.state.isLoading) {
+   if (this.props.type == 'active') {
+    classrooms = (
+     <div className="spinner-container"></div>
+     )
+   }
+  } else {
+   classrooms = this.state.classrooms.map(function(classroom) {
+    return (
+     <ClassroomContainer classroom = {classroom}
+     key = {classroom["id"]} />
+     );
+   });
 
-            if (this.props.type == 'active') {
-                createCourse = <ClassroomCreationModal teacher_id={this.props.teacher_id} />
-            }
-        }
+   if (this.props.type == 'active') {
+    createCourse = <ClassroomCreationModal teacher_id={this.props.teacher_id} />
+   }
+  }
 
-        return (
-            <div>
-                { classrooms }
-                { createCourse }
-            </div>
-        );
-    }
+  return (
+   <div>
+   { classrooms }
+   { createCourse }
+   </div>
+   );
+ }
 }
 
 TeacherModal.propTypes = {
-    teacher_id : React.PropTypes.number.isRequired,
-    program_id : React.PropTypes.string.isRequired,
-    type       : React.PropTypes.string.isRequired
+ teacher_id : React.PropTypes.number.isRequired,
+ program_id : React.PropTypes.string.isRequired,
+ type    : React.PropTypes.string.isRequired
 };
 
 /**
  * @prop classroom - the info about this classroom
  */
-class ClassroomContainer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+ class ClassroomContainer extends React.Component {
+ constructor(props) {
+  super(props);
+ }
 
-    render() {
-        const classType = `classroom-${this.props.classroom.program.id}`
-        return (
-            <div className="classroom-card-col">
-                <div className={`classroom-card ${classType}`}>
-                    <a href={`/classrooms/${this.props.classroom.id}`}>
-                        <div className="name-container">
-                            <h1 className="title">{ this.props.classroom.name }</h1>
-                            <h2 className="program">{ this.props.classroom.program.name }</h2>
-                        </div>
-                        <div className="count-container">
-                            <h3 className="count">{ this.props.classroom.students.length }</h3>
-                            <h2 className="student-label">students</h2>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        );
-    }
+ render() {
+  const classType = `classroom-${this.props.classroom.program.id}`
+  return (
+   <div className="classroom-card-col">
+   <div className={`classroom-card ${classType}`}>
+   <a href={`/classrooms/${this.props.classroom.id}`}>
+   <div className="name-container">
+   <h1 className="title">{ this.props.classroom.name }</h1>
+   <h2 className="program">{ this.props.classroom.program.name }</h2>
+   </div>
+   <div className="count-container">
+   <h3 className="count">{ this.props.classroom.students.length }</h3>
+   <h2 className="student-label">students</h2>
+   </div>
+   </a>
+   </div>
+   </div>
+   );
+ }
 }
 
 
