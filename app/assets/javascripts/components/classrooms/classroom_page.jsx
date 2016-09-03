3310d60f29b35c65e4369e9500e08d694a42e01a
Aleks Kamko <aykamko@gmail.com>
2016-09-03 15:03:13 -0700

    onboarding fix for admin
    Summary: don't show the onboarding modal for new admin users
    
    Test Plan: login as admin and see classroom page
    
    Reviewers: aleks
    
    Reviewed By: aleks
    
    Subscribers: bigblue, neezus, generic, caseytaco
    
    Projects: #bizworld
    
    Differential Revision: http://phab.calblueprint.org/D625

--- app/assets/javascripts/components/classrooms/classroom_page.jsx
+++ app/assets/javascripts/components/classrooms/classroom_page.jsx
@@ -6,54 +6,54 @@
  */
 class ClassroomPage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { classroom : { }, isLoading : true };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { classroom : { }, isLoading : true };
+  }
 
-    componentDidMount() {
-        this._fetchClassroom();
-    }
+  componentDidMount() {
+    this._fetchClassroom();
+  }
 
-    _fetchClassroom = () => {
-        const success = (data) => {
-            this.setState({ classroom: data, isLoading : false })
-        }
-        APIRequester.getJSON(APIConstants.classrooms.member(
-            this.props.classroom_id), success);
+  _fetchClassroom = () => {
+    const success = (data) => {
+      this.setState({ classroom: data, isLoading : false })
     }
+    APIRequester.getJSON(APIConstants.classrooms.member(
+      this.props.classroom_id), success);
+  }
 
-    render() {
-        let classpage, onboard;
+  render() {
+    let classpage, onboard;
 
-        if (this.props.onboarding) {
-            onboard = <ClassroomOnboard teacher_id = {this.props.teacher_id} />
-        }
-        if (this.state.isLoading) {
-            classpage = (
-                <div className="spinner-container"></div>
-            )
-        } else {
-            classpage = (
-                <div>
-                    { onboard }
-                    <ClassInfo classroom    = {this.state.classroom}
-                               success      = {this._fetchClassroom}
-                               isAdmin      = {this.props.isAdmin} />
-                    <StudentsTable students     = {this.state.classroom.students}
-                                   classroom_id = {this.props.classroom_id}
-                                   success      = {this._fetchClassroom} />
-                </div>
-            );
-        }
-        return (
-            <div>
-                { classpage }
-            </div>
-        );
+    if (this.props.onboarding && !this.props.isAdmin) {
+      onboard = <ClassroomOnboard teacher_id = {this.props.teacher_id} />
     }
+    if (this.state.isLoading) {
+      classpage = (
+        <div className="spinner-container"></div>
+      )
+    } else {
+      classpage = (
+        <div>
+          { onboard }
+          <ClassInfo classroom  = {this.state.classroom}
+                 success    = {this._fetchClassroom}
+                 isAdmin    = {this.props.isAdmin} />
+          <StudentsTable students   = {this.state.classroom.students}
+                   classroom_id = {this.props.classroom_id}
+                   success    = {this._fetchClassroom} />
+        </div>
+      );
+    }
+    return (
+      <div>
+        { classpage }
+      </div>
+    );
+  }
 }
 
 ClassroomPage.propTypes = {
-    classroom_id: React.PropTypes.number
+  classroom_id: React.PropTypes.number
 };
